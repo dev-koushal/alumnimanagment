5d66e5a4f629588c4a90362032e54a315c64b1e9
dev-koushal <koushalydv99@gmail.com>
2025-09-03 09:45:43 +0530

    feature: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
-
+import { inject } from '@vercel/analytics';
+inject();
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
