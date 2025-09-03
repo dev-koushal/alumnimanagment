ca99ebdf14285e4d546525daa8680591ca44c31f
dev-koushal <koushalydv99@gmail.com>
2025-09-03 10:17:03 +0530

    fix: minor bug fixes

--- src/Components/LoginPage/StudentLogin.jsx
+++ src/Components/LoginPage/StudentLogin.jsx
@@ -5,7 +5,7 @@ const StudentLogin = () => {
   return (
     <>
       <div className="h-screen flex justify-center items-center">
-        <fieldset className="fieldset bg-base-200 border-base-300 m-2 md:m-0 rounded-box w-md h-[350px] border p-6">
+        <fieldset className="fieldset bg-base-200 border-base-300 m-2 md:m-0 rounded-box w-md h-[400px] border p-6">
           <h1 className="text-2xl">Student Login</h1> 
           <label className="label">Email</label>
           <input type="email" className="input w-full" placeholder="Email" />
@@ -13,7 +13,8 @@ const StudentLogin = () => {
           <label className="label">Password</label>
           <input type="password" className="input w-full" placeholder="Password" />
 
-          <button className="btn btn-neutral mt-4"><Link to='/student/profile'>Login</Link></button>
+          <button className="btn btn-neutral mt-4"><Link to='/student/profile'> Bypass Login</Link></button>
+          <button className="btn btn-neutral mt-4">Login</button>
           <button className="btn btn-neutral mt-4"> Register with Google</button>
         </fieldset>
       </div>
