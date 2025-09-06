62e4012e354f6e7d8d8e4ac7659cd41b60a7a206
dev-koushal <koushalydv99@gmail.com>
2025-09-06 10:00:45 +0530

    fix: logout

--- src/Components/StudentPage/StudentExplorePage.jsx
+++ src/Components/StudentPage/StudentExplorePage.jsx
@@ -22,11 +22,11 @@ const FilterYear = ()=>{
   return (
     <>
         
-        <div className="w-full flex justify-end gap-4 items-center bg-neutral">
-          <button className="btn bg-white text-black border-[#e5e5e5]">
+        <div className="w-full flex justify-between gap-4 items-center bg-neutral">
+          <Link to='/'><button className="btn ml-2  bg-white text-black border-[#e5e5e5]">
         <svg aria-label="Email icon" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="black"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></g></svg>
-       <Link to='/'>Logout</Link>
-        </button>
+       Logout
+        </button></Link>
             <StudenProfileButton />
         </div>
          <div className="bg-neutral min-h-screen">
@@ -37,9 +37,9 @@ const FilterYear = ()=>{
         <input onClick={()=>FilterYear()} className="btn rounded-2xl" type="checkbox" name="frameworks" aria-label="2025"/>
         
         </form>
-        
+        {/* cardddddesssss */}
 
-        <div className=" flex gap-10  md:flex-row flex-wrap justify-center m- 1">
+        <div className=" flex gap-10  md:flex-row flex-wrap justify-center m-1">
         {filterCard.map((alumni, index) => (
           <div key={index}>
             <div className="card bg-base-100 md:w-96 w-80 hover:scale-[1.01] transition-all ease-in-out duration-300 shadow-md shadow-gray-600 hover:shadow-gray-400">
@@ -58,7 +58,7 @@ const FilterYear = ()=>{
                    {alumni.branch} ● {alumni.yog} ● {alumni.city} 
                 </p>
                 <div className="card-actions">
-                  <button className="btn btn-primary hover:scale-[1.1] -mb-10 rounded-full">Connect</button>
+                  <button className="btn btn-primary hover:scale-[1.1] -mb-10 rounded-full hover:shadow-md shadow-red-200 transition ease-in-out  duration-300">Connect</button>
                 </div>
               </div>
             </div>
